8daf975ee743ee1538f2b207e8b71062ace1fa16
Kwame Fosu-Gyiabour <kfosu-gyiabour@st.ug.edu.gh>
2026-07-28 12:11:13 -0700

    Defined funcctions for sum, average, maximum and minimum

--- assignment_03_array_statistics.py
+++ assignment_03_array_statistics.py
@@ -38,4 +38,51 @@
 # =============================================================================
 # YOUR CODE BELOW — remove the # symbols from the scaffold and fill it in
 # =============================================================================
+def calculate_sum(numbers):
+    total = 0
+    for num in numbers:
+        total += num
+    return total
+
+def calculte_average(numbers):
+    total = calculate_sum(numbers)
+    return total / len(numbers)
+
+def calculate_maximum(numbers):
+    max_val = numbers[0]
+    for num in numbers:
+        if num > max_val:
+            max_val = num
+    return max_val
+
+def calculate_minimum(numbers):
+    min_val = numbers[0]
+    for num in numbers:
+        if num < min_val:
+            min_val = num
+    return min_val
+
+def main():
+    raw_input = input("How many numbers?")
+    if raw_input.isdigit() and int(raw_input) > 0:
+        count = int(raw_input)
+    else:
+        print("Error: Input must be a posoitive integer.")
+        return
+
+    numbers = []
+    for i in range(1, count + 1):
+        num = float(input(f"Enter number{i}: "))
+        numbers.append(num)
+      
+
+    print("\nResults:")
+    print(f"Sum:         {calculate_sum(numbers)}")
+    print(f"Average   {calculte_average(numbers)}")
+    print(f"Maximum: {calculate_maximum(numbers)}")
+    print(f"Minimum: {calculate_minimum(numbers)}")
+
+if __name__ == "__main__":
+    main()
+
 
